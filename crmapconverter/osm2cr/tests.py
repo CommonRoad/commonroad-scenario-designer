67938e6a8bfd427479ca00c6c80a4eaf94f0b35c
Benjamin Orthen <benjamin@orthen.net>
2019-06-21 22:58:15 +0200

    Deleted old, unnecessary files

--- crmapconverter/osm2cr/tests.py
+++
@@ -1,7 +0,0 @@
-"""
-This module was used in the development process and should be ignored.
-"""
-from crmapconverter.osm2cr.converter_modules.gui_modules import gui_embedding
-
-app = gui_embedding.MainApp()
-app.start()
