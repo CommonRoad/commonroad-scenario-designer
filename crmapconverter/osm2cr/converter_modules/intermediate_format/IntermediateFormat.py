5ed078fd279904fef754cbcc75c270354f1a67dc
Behtarin Ferdousi <behtarin.ferdousi@tum.de>
2019-12-02 15:37:18 +0100

    Move Traffic Rule related classes and methods to utilities

--- crmapconverter/osm2cr/converter_modules/intermediate_format/IntermediateFormat.py
+++ crmapconverter/osm2cr/converter_modules/intermediate_format/IntermediateFormat.py
@@ -1,32 +1,13 @@
 """
 This module holds the classes required for the intermediate format
 """
-from enum import Enum
 
 from commonroad.scenario.obstacle import Obstacle
 from typing import List, Set
 from pyparsing import Dict
 
 from crmapconverter.osm2cr.converter_modules.utility import geometry
-
-
-class Direction(Enum):
-    """
-    Enum for all the possible directions for a traffic signal
-    """
-    LEFT = "left"
-    RIGHT = "right"
-    STRAIGHT = "straight"
-
-
-class Signal(Enum):
-    """
-    Enum for the possible types of traffic light in signals
-    """
-    RED: "red"
-    YELLOW: "yellow"
-    REDYELLOW: "redYellow"
-    GREEN: "green"
+from crmapconverter.osm2cr.converter_modules.utility.traffic_rules import TrafficSign
 
 
 class Edge:
@@ -48,55 +29,6 @@ class Edge:
         self.rightBound = right_bound
         self.maxspeed = additional_values['maxspeed']
         self.width = additional_values['width']
-
-
-class TrafficSign:
-    """
-    Class to represent Traffic Signs
-    """
-
-    def __init__(self,
-                 id: str,
-                 position: geometry.Point,
-                 additional_values: Dict):
-        """
-
-        :param id: string for id of the sign: COUNTRYCODE:id
-        :param position: location of the signal
-        :param additional_values: key-value pair of additional values
-        """
-        self.id = id
-        self.position = position
-        self.additional_values = additional_values
-
-
-class TrafficSignal(TrafficSign):
-    """
-    Class to represent the traffic signals
-    """
-
-    def __init__(self,
-                 id: str,
-                 position: geometry.Point,
-                 cycle: List[(Signal, int)],
-                 offset: int,
-                 direction: Direction,
-                 active: bool):
-        """
-
-        :param id: string for the id
-        :param position: point for the location
-        :param cycle: list of tuple of signal and respective time segment
-        :param offset: time offset for the signal cycle
-        :param direction: applicable direction
-        :param active: is active or not
-        """
-        self.id = id
-        self.position = position
-        self.cycle = cycle
-        self.offset = offset
-        self.direction = direction
-        self.active = active
 
 
 class IntermediateFormat:
