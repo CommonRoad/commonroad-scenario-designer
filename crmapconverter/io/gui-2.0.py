d0c5f4106bfe6117424a181b73aac0accb65bcb1
ryanzbt <ryan.zbt@hotmail.fr>
2020-01-09 13:54:28 +0100

    Creating Design class for Buttons

--- crmapconverter/io/gui-2.0.py
+++ crmapconverter/io/gui-2.0.py
@@ -2,7 +2,20 @@ import os
 import signal
 import sys
 
-import crmapconverter.io.gui as G
+try:
+    import crmapconverter.io.gui as G
+    from commonroad.common.file_writer import CommonRoadFileWriter
+    from commonroad.common.file_reader import CommonRoadFileReader
+
+    from crmapconverter.osm.osm2lanelet import OSM2LConverter
+    from crmapconverter.osm.parser import OSMParser
+    from crmapconverter.osm.lanelet2osm import L2OSMConverter
+
+    from crmapconverter.opendriveparser.parser import parse_opendrive
+    from crmapconverter.opendriveconversion.network import Network
+    from crmapconverter.io.viewer import MainWindow as ViewerWidget
+except:
+    print("You need to install manually commonroad")
 
 from tkinter import *
 from tkinter import filedialog
@@ -10,24 +23,31 @@ from tkinter.messagebox import *
 from PIL import Image
 from PIL import ImageTk
 
-from commonroad.common.file_writer import CommonRoadFileWriter
-from commonroad.common.file_reader import CommonRoadFileReader
-
-from crmapconverter.osm.osm2lanelet import OSM2LConverter
-from crmapconverter.osm.parser import OSMParser
-from crmapconverter.osm.lanelet2osm import L2OSMConverter
-
-from crmapconverter.opendriveparser.parser import parse_opendrive
-from crmapconverter.opendriveconversion.network import Network
-from crmapconverter.io.viewer import MainWindow as ViewerWidget
 
 try:
     from crmapconverter.osm2cr.main import start_gui
 except ModuleNotFoundError as module_err:
     print(module_err)
     print("It seems like you did not install the dependencies for osm2cr.")
+except:
+    print("You need to install manually commonroad")
 
 
+class Design(Button):
+    """Class defining the design of the buttons used in the interface defined below"""
+
+    def __init__(self, parent, row, column, icon, command=print("ok"), **kwargs):
+        self.row = row
+        self.column = column
+        self.icon = icon
+        self.command = command
+        Button.__init__(self, parent, **kwargs)
+        self.configure(image=icon)
+        self.configure(borderwidth=0)
+        self.configure(activebackground='#003359')
+        self.configure(background='#003359')
+        self.configure(cursor='hand2')
+        self.grid(column=self.column, row=self.row)
 
 
 class Interface(Frame):
@@ -35,12 +55,14 @@ class Interface(Frame):
 
     # osm2cr
     def osm2cr(self):
-        start_gui()
+        return 1
+        #start_gui()
 
     # OpenDrive2Lanelet
     def opendrive2Lanelet(self):
         """Open the conversion tool for OpenDRIVE in a new window."""
-        G.opendrive_gui()
+        return 2
+        #G.opendrive_gui()
 
     def __init__(self, fenetre, **kwargs):
         Frame.__init__(self, fenetre, bg="#003359", **kwargs)
@@ -69,41 +91,27 @@ class Interface(Frame):
 
         b1_src_pil = Image.open("gui_src/Groupe 1.png")
         b1_src = ImageTk.PhotoImage(b1_src_pil)
-        b1 = Button(button_canvas_1, command=self.osm2cr, image=b1_src, borderwidth=0, activebackground=active_color,
-                    background='#003359', cursor='hand2')
-        b1.image = b1_src
-        b1.grid(row=2, column=0, sticky=W)
+        b1 = Design(button_canvas_1, 2, 0, b1_src)
 
         b2_src_pil = Image.open("gui_src/Groupe 2.png")
         b2_src = ImageTk.PhotoImage(b2_src_pil)
-        b2 = Button(button_canvas_1, image=b2_src, borderwidth=0, activebackground=active_color, background='#003359',
-                    cursor='hand2')
-        b2.image = b2_src
-        b2.grid(row=2, column=1)
+        b2 = Design(button_canvas_1, 2, 1, b2_src)
 
         b3_src_pil = Image.open("gui_src/Groupe 3.png")
         b3_src = ImageTk.PhotoImage(b3_src_pil)
-        b3 = Button(button_canvas_1, command=self.opendrive2Lanelet, image=b3_src, borderwidth=0,
-                    activebackground=active_color, background='#003359', cursor='hand2')
-        b3.image = b3_src
-        b3.grid(row=2, column=2, sticky=W)
+        b3 = Design(button_canvas_1, 2, 2, b3_src)
+        b3.grid(row=2, column=2)
 
-        button_canvas_2 = Canvas(self)
+        button_canvas_2 = Canvas(self, background='#003359', highlightbackground = '#003359')
         button_canvas_2.grid(row=3, pady=30)
 
         b4_src_pil = Image.open("gui_src/Groupe 4.png")
         b4_src = ImageTk.PhotoImage(b4_src_pil)
-        b4 = Button(button_canvas_2, padx=10, image=b4_src, borderwidth=0, activebackground=active_color, background='#003359',
-                    cursor='hand2')
-        b4.image = b4_src
-        b4.grid(row=3, column=0)
+        b4 = Design(button_canvas_2, 3, 0, b4_src, padx= 40)
 
         b5_src_pil = Image.open("gui_src/Groupe 5.png")
         b5_src = ImageTk.PhotoImage(b5_src_pil)
-        b5 = Button(button_canvas_2, image=b5_src, borderwidth=0, activebackground=active_color, background='#003359',
-                    cursor='hand2')
-        b5.image = b5_src
-        b5.grid(row=3, column=1)
+        b5 = Design(button_canvas_2, 3, 1, b5_src, padx=40)
 
 
 # INITIALISATION
@@ -121,6 +129,5 @@ welcome.resizable(False, False)
 
 
 interface = Interface(welcome)
-i = Interface(welcome)
 interface.mainloop()
-i.mainloop()
+
