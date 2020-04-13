24bbaef2ac91a53b26e9e41b3b0da517243dc778
Fabian Höltke <fabian.h001@gmail.com>
2020-04-13 19:04:30 +0200

    enable traffic signs again

--- crmapconverter/osm2cr/converter_modules/intermediate_format/intermediate_format.py
+++ crmapconverter/osm2cr/converter_modules/intermediate_format/intermediate_format.py
@@ -411,8 +411,6 @@ class IntermediateFormat:
             net.add_lanelet(lanelet)
 
         # Add Traffic Signs
-        """
-        TODO Add Traffic signs
         for sign in self.traffic_signs:
             net.add_traffic_sign(sign, set())
 
@@ -423,7 +421,7 @@ class IntermediateFormat:
         # Add Intersections
         for intersection in self.intersections:
             net.add_intersection(intersection)
-        """
+
         # TODO Add Obstacles
 
         scenario.lanelet_network = net
