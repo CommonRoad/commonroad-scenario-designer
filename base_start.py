bff3d7d637952401d325e611bdfe94ea6207348d
Max Winklhofer <winklhofer@tufast.de>
2020-06-19 10:17:47 +0200

    add osm test files

--- base_start.py
+++ base_start.py
@@ -1,12 +1,11 @@
-import sys
-import os
-sys.path.append(os.getcwd())
-sys.path.append('/home/max/Desktop/Planning/commonroad-io')
-
-from crmapconverter.io.visualize_commonroad import main
+#from crmapconverter.io.visualize_commonroad import main
 # from crmapconverter.io.gui import main
 # from crmapconverter.io.viewer import main
-from crmapconverter.osm2cr.main import convert
+#from crmapconverter.osm2cr.main import convert
+from crmapconverter.osm2cr.converter_modules import converter
 
 # convert("../Maps/mittlerer_Ring.osm")
-main()
+
+scenario_file = "/home/max/Desktop/Planning/Maps/osm_files/only_straigt_test.osm"
+
+converter.Scenario(scenario_file)
