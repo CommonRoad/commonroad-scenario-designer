fa20f58e7870c6caac07149dcf89330852e006eb
Max Winklhofer <winklhofer@tufast.de>
2020-07-04 21:51:29 +0200

    readd dependancie

--- crmapconverter/io/V3_0/gui_sumo_simulation.py
+++ crmapconverter/io/V3_0/gui_sumo_simulation.py
@@ -27,7 +27,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 
-#from scenarios.a9.scenario_config import Conf
+from scenarios.a9.scenario_config import Conf
 
 
 class MyDynamicCanvas(FigureCanvas):
