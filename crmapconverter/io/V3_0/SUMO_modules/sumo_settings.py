d3b58c51829c4850f5a6ea41e601f0c14161a036
Max Winklhofer <ge34fub@mytum.de>
2020-07-19 10:59:26 +0200

    Update sumo_settings.py

--- crmapconverter/io/V3_0/SUMO_modules/sumo_settings.py
+++ crmapconverter/io/V3_0/SUMO_modules/sumo_settings.py
@@ -1,6 +1,6 @@
 import sys
 from crmapconverter.io.V3_0.GUI_resources.sumo_settings_ui import Ui_MainWindow
-from crmapconverter.sumo_map import config
+from crmapconverter.sumo_map.config import SumoConfig as config
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -29,36 +29,48 @@ class SUMOSettings:
         :return: None
         """
         # line edits
-        self.window.le_ego_ids.setText(config.
-        self.window.lineEdit_departure_interval_vehicles.setText(config.
+        # self.window.le_ego_ids.setText(config.ego_ids) # List but why?
+        # self.window.lineEdit_departure_interval_vehicles.setText(
+        #     config.departure_interval_vehicles ) # Intervall
+            
         # spin boxes
-        self.window.spinBox.setValue(config.
-        self.window.sb_random_seed.setValue(config.
-        self.window.sb_2.setValue(config.
-        self.window.sb_ego_start_time.setValue(config.
-        self.window.sb_num_ego_vehicles.setValue(config.
-        self.window.sb_overwrite_speed_limit.setValue(config.
-        self.window.sb_lanelet_check_time_window.setValue(config.
-        self.window.sb_unrestricted_max_speed_default.setValue(config.
-        self.window.sb_delta_steps.setValue(config.
-        self.window.sb_n_vehicles_max.setValue(config.
-        self.window.sb_consistency_window.setValue(config.
-        self.window.sb_max_veh_per_km.setValue(config.
-        self.window.sb_veh_per_second.setValue(config.
-        self.window.sb_unrestricted_speed_limit_default.setValue(config.
-        self.window.sb_fringe_factor.setValue(config.
-        self.window.sb_wait_pos_internal_junctions.setValue(config.
-        self.window.sb_ego_veh_width.setValue(config.
-        self.window.sb_protection_margin.setValue(config.
-        self.window.sb_lane_change_tol.setValue(config.
-        self.window.sb_vehicle_length_interval.setValue(config.
-        self.window.sb_ego_veh_length.setValue(config.
-        self.window.sb_vehicle_width_interval.setValue(config.
-        self.window.sb_passenger.setValue(config.
-        self.window.sb_truck.setValue(config.
-        self.window.sb_bus.setValue(config.
-        self.window.sb_bicycle.setValue(config.
-        self.window.sb_pedestrian.setValue(config.
+        # self.window.spinBox.setValue(config.
+        self.window.sb_random_seed.setValue(config.random_seed)
+        # self.window.sb_2.setValue(config.
+        self.window.sb_ego_start_time.setValue(config.ego_start_time)
+        self.window.sb_num_ego_vehicles.setValue(config.n_ego_vehicles) # rename
+        self.window.sb_overwrite_speed_limit.setValue(
+            config.overwrite_speed_limit)
+        self.window.sb_lanelet_check_time_window.setValue(
+            config.lanelet_check_time_window)
+        self.window.sb_unrestricted_max_speed_default.setValue(
+            config.unrestricted_max_speed_default)
+        self.window.sb_delta_steps.setValue(config.delta_steps)
+        self.window.sb_n_vehicles_max.setValue(config.n_vehicles_max)
+        self.window.sb_consistency_window.setValue(config.consistency_window)
+        self.window.sb_max_veh_per_km.setValue(config.max_veh_per_km)
+        self.window.sb_veh_per_second.setValue(config.veh_per_second)
+        self.window.sb_unrestricted_speed_limit_default.setValue(
+            config.unrestricted_speed_limit_default)
+        self.window.sb_fringe_factor.setValue(config.fringe_factor)
+        self.window.sb_wait_pos_internal_junctions.setValue(
+            config.wait_pos_internal_junctions)
+        self.window.sb_ego_veh_width.setValue(config.ego_veh_width)
+        self.window.sb_protection_margin.setValue(config.protection_margin)
+        self.window.sb_lane_change_tol.setValue(config.lane_change_tol)
+        self.window.sb_vehicle_length_interval.setValue(
+            config.vehicle_length_interval)
+        self.window.sb_ego_veh_length.setValue(config.ego_veh_length)
+        self.window.sb_vehicle_width_interval.setValue(
+            config.vehicle_width_interval)
+        # TODO veh dist to extra window
+        self.window.sb_passenger.setValue(config.veh_distribution['passenger'])
+        self.window.sb_truck.setValue(config.veh_distribution['truck'])
+        self.window.sb_bus.setValue(config.veh_distribution['bus'])
+        self.window.sb_bicycle.setValue(config.veh_distribution['bicycle'])
+        self.window.sb_pedestrian.setValue(
+            config.veh_distribution['pedestrian'])
+            
         # combo boxes
         # comboBox_lane_change_window.
         # comboBox_compute_orientation
@@ -69,36 +81,37 @@ class SUMOSettings:
 
         :return: None
         """
-        config. = le_ego_ids.text()
-        config. = lineEdit_departure_interval_vehicles.text()
+        # config.ego_ids = le_ego_ids.text()
+        # config.departure_interval_vehicles = lineEdit_departure_interval_vehicles.text()
         # spin boxes
-        config. = sb.value()
-        config. = sb_random_seed.value()
-        config. = sb_2.value()
-        config. = sb_ego_start_time.value()
-        config. = sb_num_ego_vehicles.value()
-        config. = sb_overwrite_speed_limit.value()
-        config. = sb_lanelet_check_time_window.value()
-        config. = sb_unrestricted_max_speed_default.value()
-        config. = sb_delta_steps.value()
-        config. = sb_n_vehicles_max.value()
-        config. = sb_consistency_window.value()
-        config. = sb_max_veh_per_km.value()
-        config. = sb_veh_per_second.value()
-        config. = sb_unrestricted_speed_limit_default.value()
-        config. = sb_fringe_factor.value()
-        config. = sb_wait_pos_internal_junctions.value()
-        config. = sb_ego_veh_width.value()
-        config. = sb_protection_margin.value()
-        config. = sb_lane_change_tol.value()
-        config. = sb_vehicle_length_interval.value()
-        config. = sb_ego_veh_length.value()
-        config. = sb_vehicle_width_interval.value()
-        config. = sb_passenger.value()
-        config. = sb_truck.value()
-        config. = sb_bus.value()
-        config. = sb_bicycle.value()
-        config. = sb_pedestrian.value()
+        # config. = sb.value() # ?
+        config.random_seed = sb_random_seed.value()
+        # config. = sb_2.value() # ?
+        config.ego_start_time = sb_ego_start_time.value()
+        configsb_n_ego_vehicles. = sb_num_ego_vehicles.value()
+        config.overwrite_speed_limit = sb_overwrite_speed_limit.value()
+        config.lanelet_check_time_window = sb_lanelet_check_time_window.value()
+        config.unrestricted_max_speed_default = sb_unrestricted_max_speed_default.value()
+        config.delta_steps = sb_delta_steps.value()
+        config.n_vehicles_max = sb_n_vehicles_max.value()
+        config.consistency_window = sb_consistency_window.value()
+        config.max_veh_per_km = sb_max_veh_per_km.value()
+        config.veh_per_second = sb_veh_per_second.value()
+        config.unrestricted_speed_limit_default = sb_unrestricted_speed_limit_default.value()
+        config.fringe_factor = sb_fringe_factor.value()
+        config.wait_pos_internal_junctions = sb_wait_pos_internal_junctions.value()
+        config.ego_veh_width = sb_ego_veh_width.value()
+        config.protection_margin = sb_protection_margin.value()
+        config.lane_change_tol = sb_lane_change_tol.value()
+        config.vehicle_length_interval = sb_vehicle_length_interval.value()
+        config.ego_veh_length = sb_ego_veh_length.value()
+        config.vehicle_width_interval = sb_vehicle_width_interval.value()
+        # TODO to extra window
+        config.passenger = sb_passenger.value()
+        config.truck = sb_truck.value()
+        config.bus = sb_bus.value()
+        config.bicycle = sb_bicycle.value()
+        config.pedestrian = sb_pedestrian.value()
         # combo boxes
         # config. = comboBox_lane_change_window
         # config. = comboBox_compute_orientation
@@ -118,6 +131,6 @@ class SUMOSettings:
 
         :return: None
         """
-        for var_name in dir(config_default):
-            if not var_name.startswith('__'):
-                setattr(config, var_name, getattr(config_default, var_name))
+        # for var_name in dir(config_default):
+        #     if not var_name.startswith('__'):
+        #         setattr(config, var_name, getattr(config_default, var_name))
