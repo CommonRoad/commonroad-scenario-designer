53cfb02ee8f5730cb33e9b4e49e98da7033dd28d
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2020-08-24 20:41:42 +0200

    add missing SUMO/OSM test files and update setup.py

--- test/test_opendrive_files.py
+++ test/test_opendrive_files.py
@@ -5,6 +5,7 @@
 import os
 import unittest
 from lxml import etree
+import time
 
 from commonroad.common.file_writer import CommonRoadFileWriter, OverwriteExistingFile
 from commonroad.planning.planning_problem import PlanningProblemSet
@@ -83,12 +84,19 @@ class TestOpenDriveBaseClass:
             writer.write_to_file(self.out_path + "/" + self.xml_output_name + ".xml", OverwriteExistingFile.ALWAYS)
 
             # set same date so this won't change the comparison
-            tree_import.set("date", "2020-04-14")
-            writer.root_node.set("date", "2020-04-14")
+            date = time.strftime("%Y-%m-%d", time.localtime())
+            tree_import.set("date", date)
+            writer.root_node.set("date", date)
 
             # compare both element trees
             trees_are_equal = elements_equal(tree_import, writer.root_node)
             self.assertTrue(trees_are_equal)
+
+class TestBasicOpenDrive(TestOpenDriveBaseClass, unittest.TestCase):
+    """Basic test with a junction in the middle."""
+
+    __test__ = False
+    xodr_file_name = "opendrive-1"
 
 
 class TestSuedTangente(TestOpenDriveBaseClass, unittest.TestCase):
@@ -98,13 +106,6 @@ class TestSuedTangente(TestOpenDriveBaseClass, unittest.TestCase):
 
     __test__ = False
     xodr_file_name = "KA-Suedtangente-atlatec"
-
-
-class TestBasicOpenDrive(TestOpenDriveBaseClass, unittest.TestCase):
-    """Basic test with a junction in the middle."""
-
-    __test__ = False
-    xodr_file_name = "opendrive-1"
 
 
 class TestCulDeSac(TestOpenDriveBaseClass, unittest.TestCase):
