6b9f580e994b295cd286d31d4eff829a9d6f1a3d
Max Fruehauf <max.fruehauf@tum.de>
2021-01-05 13:34:32 +0100

    Update CI image

--- ci/build.sh
+++ ci/build.sh
@@ -3,8 +3,8 @@
 version=$1
 gitlab_img=gitlab.lrz.de:5005/cps/commonroad-map-tool/ci:$version
 
-#DOCKER_BUILDKIT=1 docker build -t "commonroad:$version" -f Dockerfile --no-cache .. \
-echo "Runnning tests ..." \
+DOCKER_BUILDKIT=1 docker build -t "commonroad:$version" -f Dockerfile --no-cache .. \
+&& echo "Runnning tests ..." \
 && docker run --rm --volume "$PWD/test.sh:/home/cruser/test.sh" \
                    --volume "$PWD/../:/home/cruser/commonroad-map-tool" \
                    --volume "$HOME/.ssh/:/home/cruser/.ssh" \
