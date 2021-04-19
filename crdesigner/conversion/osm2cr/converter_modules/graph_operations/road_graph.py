found bug in degrees computation!

crdesigner/conversion/osm2cr/converter_modules/graph_operations/road_graph.py
@@ -301,15 +301,6 @@ class GraphNode:
                     "malformed graph, node has edges assigned to it, which start elsewhere"
                 )
 
-    def add_traffic_sign(self, sign: "GraphTrafficSign"):
-        # this method is never called
-        for edge in self.edges:
-            for lane in edge.lanes:
-                # add to forward lanes
-                # TODO determine in which direction
-                if lane.forward:
-                    lane.add_traffic_sign(sign)
-
 
 class GraphEdge:
     """
@@ -477,10 +468,23 @@ class GraphEdge:
         calculates the compass degrees of an edge as in https://en.wikipedia.org/wiki/Points_of_the_compass#/media/File:Compass_Card_B+W.svg
         :return: compass orientation in degrees
         """
-        edge_compass_degrees = math.degrees(self.get_orientation(self.node1)) - 45
-        if edge_compass_degrees < 0.0:
-            edge_compass_degrees+= 360.0
-        return edge_compass_degrees
+     
+        # compute radians
+        delta_x = self.node2.x - self.node1.x
+        delta_y = self.node2.y - self.node1.y
+        radians = np.arctan2(delta_y, delta_x)  
+    
+        # black math magic from https://stackoverflow.com/a/7805311
+        if radians < 0:
+            radians = abs(radians)
+        else:
+            radians = 2 * np.pi - radians
+        degrees = math.degrees(radians)
+        degrees +=90.0
+        if degrees>360.0:
+            degrees -= 360
+        # return correctly computed degrees
+        return degrees
 
     def angle_to(self, edge: "GraphEdge", node: GraphNode) -> float:
         """
@@ -731,31 +735,12 @@ class GraphEdge:
         forward = True
         sign_direction = sign.direction
         if sign_direction is not None:
+            # get compass degrees of edge
             edge_orientation = self.get_compass_degrees()
-            # Debugging
-            # print(sign.sign)
-            # print("edge orientation: {}".format(edge_orientation))
-            # print("sign direction {} ".format(sign.direction))
             if abs(sign_direction-edge_orientation) < 180:
                 forward = False
 
-        # add traffic signs to lanes
-
-        # approach 1, works only if sign direction is provided
-        # if sign_direction is not None:
-        #     favorable_lane = self.lanes[0]
-        #     for lane in self.lanes:
-        #         print("lane degrees: "+ str(lane.get_compass_degrees()))
-        #         if abs(sign_direction - lane.get_compass_degrees()) < abs(sign_direction - favorable_lane.get_compass_degrees()) :
-        #             favorable_lane = lane
-
-        #     if abs(sign_direction - favorable_lane.get_compass_degrees()) < 70: # threshold in degrees
-        #         favorable_lane.add_traffic_sign(sign)
-        #         return
-
-        # use approach 2 if no sign direction could be provided
-        # Warning! Sometimes edge forward direction != lane forward direction, this leads to wrongfully assigned traffic signs
-        for lane in self.lanes:
+            # add sign to forward lanes
             if lane.forward and forward:
                     lane.add_traffic_sign(sign)
             # add to backward lanes