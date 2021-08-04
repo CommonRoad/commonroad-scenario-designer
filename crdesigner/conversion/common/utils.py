0d922581c34c04fd534af77867221269a939092f
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-08-04 16:34:56 +0200

    possibility to provide id for generate_unique_id

--- crdesigner/conversion/common/utils.py
+++ crdesigner/conversion/common/utils.py
@@ -7,8 +7,11 @@ __email__ = "commonroad@lists.lrz.de"
 __status__ = "Development"
 
 
-def generate_unique_id():
+def generate_unique_id(set_id: int = None):
     if not hasattr(generate_unique_id, "counter"):
         generate_unique_id.counter = 0  # it doesn't exist yet, so initialize it
+    if set_id is not None:
+        generate_unique_id.counter = set_id
+        return generate_unique_id.counter
     generate_unique_id.counter += 1
     return generate_unique_id.counter
