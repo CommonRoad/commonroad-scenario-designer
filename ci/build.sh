c7ad6bea11697c03c92b1df629a5e71820741a6d
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-09-22 10:33:15 +0200

    add sumo installation script and update docker and ci

--- ci/build.sh
+++ ci/build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-version=<version>
+version=0.6
 gitlab_img=gitlab.lrz.de:5005/cps/commonroad-scenario-designer/ci:$version
 
 DOCKER_BUILDKIT=1 docker build -t "commonroad:$version" -f Dockerfile --no-cache .. \
 && docker tag "commonroad:$version" "latest" \
-&& echo "Runnning tests ..." \
-&& docker run --rm --volume "$PWD/test.sh:/home/cruser/test.sh" \
-                   --volume "$PWD/../:/home/cruser/commonroad-map-tool" \
-                   --volume "$HOME/.ssh/:/home/cruser/.ssh" \
-        "commonroad:$version" /home/cruser/test.sh \
 && docker login gitlab.lrz.de:5005 \
 && docker tag "commonroad:$version" "$gitlab_img" \
 && docker push "$gitlab_img"
