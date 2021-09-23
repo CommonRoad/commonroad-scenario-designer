3bf43f4e2ea6505fe1085787598fa8c6268e7cbe
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-09-23 14:51:52 +0200

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,7 +74,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -112,10 +112,6 @@ html_theme_options = {
     'titles_only': False
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -131,7 +127,7 @@ html_static_path = ["_static"]
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "crdesigner"
+htmlhelp_basename = "crdesigner-doc"
 
 
 # -- Options for LaTeX output ------------------------------------------------
