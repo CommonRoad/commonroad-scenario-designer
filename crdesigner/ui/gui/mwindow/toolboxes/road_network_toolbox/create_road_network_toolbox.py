88b1901856a0ef621bcad6b5caf6a32fafb6a4dd
Ben <ge38rul@mytum.de>
2021-12-07 10:47:43 +0100

    Reformatted code.

--- crdesigner/ui/gui/mwindow/toolboxes/road_network_toolbox/create_road_network_toolbox.py
+++ crdesigner/ui/gui/mwindow/toolboxes/road_network_toolbox/create_road_network_toolbox.py
@@ -9,7 +9,7 @@ def create_road_network_toolbox(mwindow):
     mwindow.road_network_toolbox = RoadNetworkToolbox(
             current_scenario=mwindow.animated_viewer_wrapper.cr_viewer.current_scenario,
             text_browser=mwindow.crdesigner_console_wrapper.text_browser,
-            callback= lambda scenario: toolbox_callback(mwindow, scenario), tmp_folder=mwindow.tmp_folder,
+            callback=lambda scenario: toolbox_callback(mwindow, scenario), tmp_folder=mwindow.tmp_folder,
             selection_changed_callback=mwindow.animated_viewer_wrapper.cr_viewer.update_plot)
     mwindow.addDockWidget(Qt.LeftDockWidgetArea, mwindow.road_network_toolbox)
     return mwindow.road_network_toolbox  # for visibility
