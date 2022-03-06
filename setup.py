2576764ab4d02b58b997938cdc17df15665e2591
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-03-06 12:57:15 +0100

    update required commonroad-io version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         "numpy>=1.16.4",
         "lxml>=4.3.4",
-        "commonroad-io>=2020.4",
+        "commonroad-io>=2021.4",
         "pyproj>=2.2.0",
         "scipy>=1.3.0",
         "mercantile >= 1.1.3",
