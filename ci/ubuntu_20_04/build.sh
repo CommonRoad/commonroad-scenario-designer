edddf47111de90e92dfb40c36021769f2d170630
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-05-20 08:19:15 +0200

    remove style check temporarily

--- ci/ubuntu_20_04/build.sh
+++ ci/ubuntu_20_04/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=v0.6.9_20.04 #last version v0.6.9_20.04
+version=<version> #last version v0.6.9_20.04
 gitlab_img=gitlab.lrz.de:5005/cps/commonroad-scenario-designer/ci:$version
 
 DOCKER_BUILDKIT=1 docker build -t "commonroad:$version" -f Dockerfile --no-cache .. \
