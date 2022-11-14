2ba202d42b67317d0061b377a6f89d2489f1ca55
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-11-14 20:01:45 +0100

    remove cartopy from setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "enum34>=1.1.10",
         "iso3166>=1.0.1",
         "networkx>=2.5",
-        "cartopy >= 0.17.0",
+        #"cartopy >= 0.17.0",
         "omegaconf >= 2.2.2",
     ],
     extras_require={"GUI": ["matplotlib>=3.1.0"]},
