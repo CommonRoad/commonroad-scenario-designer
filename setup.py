bca00205b93c8eb9aba58cb2597025045345249d
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-12-13 20:21:45 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(exclude=("test", "ci", "files", "docs", "tutorials")),
     include_package_data=True,
     install_requires=[
-        "numpy>=1.23",
+        "numpy>=1.21.6",
         "lxml>=4.3.4",
         "pyproj>=3.4.1",
         "scipy>=1.3.0",
