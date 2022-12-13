934c8729dbdf579ba0777c2f9de847ce6f9dd75e
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-12-13 20:56:54 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         "numpy>=1.21.6",
         "lxml>=4.3.4",
-        "pyproj>=3.4.0",
+        "pyproj==3.2.1",
         "scipy>=1.3.0",
         "mercantile >= 1.1.3",
         "utm >= 0.5.0",
