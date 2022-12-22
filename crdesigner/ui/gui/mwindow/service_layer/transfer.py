85d9e8457534a7d685338c4d19bf31f3bc0f6462
Yosr Nabli <yosr.nabli99@gmail.com>
2022-12-22 10:44:36 +0100

    comment is inside the function

--- crdesigner/ui/gui/mwindow/service_layer/transfer.py
+++ crdesigner/ui/gui/mwindow/service_layer/transfer.py
@@ -1,12 +1,12 @@
 import yaml
 
-'''
-this file contains a functions that will write the settings present in the yaml file into the values of the config file
-It is in a single file because it will be called in gui_settings.py, osm_settings.py and start_gui.py
-'''
-
 
 def transfer_yaml_to_config(yaml_file: str, config: any):
+    """
+    this file contains a functions that will write the settings present in the yaml file into the values of the
+    config file
+    It is in a single file because it will be called in gui_settings.py, osm_settings.py and start_gui.py
+    """
     with open(yaml_file) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     for key, value in data.items():
