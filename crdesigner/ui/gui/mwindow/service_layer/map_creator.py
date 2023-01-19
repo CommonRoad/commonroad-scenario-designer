fixed the bug by adding an if else statement for the sign of the sin so that the angle will be calculated correctly

crdesigner/ui/gui/mwindow/service_layer/map_creator.py
@@ -298,9 +298,17 @@ class MapCreator:
                 a = predecessor.right_vertices[-1] - predecessor.center_vertices[-1]
                 b = successor.right_vertices[0] - successor.center_vertices[0]
                 inner = np.inner(a, b)
-                norms = np.linalg.norm(a) * np.linalg.norm(b)
-                cos = inner / norms
-                ang = np.arccos(np.clip(cos, -1.0, 1.0))
+                if inner == 0:
+                    sign = np.cross(a, b)
+                    if sign > 0:
+                        ang = - np.pi / 2
+                    else:
+                        ang = np.pi / 2
+                else:
+                    norms = np.linalg.norm(a) * np.linalg.norm(b)
+                    cos = inner / norms
+                    ang = np.arccos(np.clip(cos, -1.0, 1.0))
+
 
 
         successor.translate_rotate(np.array([0, 0]), ang)