ac543acaee4405491bd6d4ca8b8de0a79f8aeef2
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2024-01-09 13:10:58 +0100

    create dir in example script

--- tutorials/conversion_examples/example_commonroad_to_opendrive.py
+++ tutorials/conversion_examples/example_commonroad_to_opendrive.py
@@ -5,5 +5,7 @@ from crdesigner.map_conversion.map_conversion_interface import commonroad_to_ope
 input_path = Path.cwd().parent.parent / "tests/map_conversion/test_maps/cr2odr/ZAM_Over-1_1.xml"
 output_name = Path.cwd() / "example_files/opendrive/ZAM_Over-1_1.odr"
 
+if not (Path.cwd() / "example_files/opendrive").exists():
+    (Path.cwd() / "example_files/opendrive").mkdir(parents=True, exist_ok=True)
 
 commonroad_to_opendrive(input_path, output_name)
