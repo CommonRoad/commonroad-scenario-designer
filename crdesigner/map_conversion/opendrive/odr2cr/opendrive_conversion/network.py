709980e03b232611fb782ab99ab59bfbadc53a01
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2024-10-01 17:07:05 +0200

    format code

--- crdesigner/map_conversion/opendrive/odr2cr/opendrive_conversion/network.py
+++ crdesigner/map_conversion/opendrive/odr2cr/opendrive_conversion/network.py
@@ -60,7 +60,9 @@ from crdesigner.map_conversion.opendrive.odr2cr.opendrive_conversion.utils impor
 from crdesigner.map_conversion.opendrive.odr2cr.opendrive_parser.elements.opendrive import (
     OpenDrive,
 )
-from crdesigner.map_conversion.opendrive.odr2cr.opendrive_parser.elements.road import Road
+from crdesigner.map_conversion.opendrive.odr2cr.opendrive_parser.elements.road import (
+    Road,
+)
 
 
 def update_line_markings(lanelet_network: ConversionLaneletNetwork) -> ConversionLaneletNetwork:
