3b2ca6960414403b11e4e6fbbc0cfc51c6cc8e92
Alberto Giacalone <alberto.giacalone@niulinx.ai>
2026-06-12 14:25:10 +0200

    Only create imagery folder when needed

--- crdesigner/ui/gui/utilities/aerial_data.py
+++ crdesigner/ui/gui/utilities/aerial_data.py
@@ -26,9 +26,6 @@ IMAGE_RESOLUTION = 256
 
 bing_maps_api_response = None
 
-os.makedirs(config.IMAGE_SAVE_PATH, exist_ok=True)
-
-
 def store_tile(quadkey: str, image: JpegImageFile) -> None:
     """
     This is useful for low internet connection speeds.
@@ -37,6 +34,7 @@ def store_tile(quadkey: str, image: JpegImageFile) -> None:
     :param image: the image
     :return: None
     """
+    os.makedirs(config.IMAGE_SAVE_PATH, exist_ok=True)
     file = config.IMAGE_SAVE_PATH + quadkey + ".jpeg"
     image.save(file, "JPEG")
     return
